89939c2ea21727127f56acaf920b56df19fed992
laptop <amnezia@thinkpad.localdomain>
2025-09-25 15:17:09 +0300

    add keyboard

---
+++ bot/app/keyboard.py
@@ -0,0 +1,6 @@
+from aiogram.types import ReplyKeyboardMarkup, KeyboardButton
+
+main = ReplyKeyboardMarkup(keyboard = [
+    [KeyboardButton(text='Meny')]
+],
+                           resize_keyboard = True)
--- bot/run.py
+++ bot/run.py
@@ -6,13 +6,14 @@ import asyncio
 from aiogram.filters import CommandStart
 from aiogram.types import Message
 load_dotenv()
+import app.keyboard as kb
 
 
 bot = Bot(os.getenv('token'))
 dp = Dispatcher()
 @dp.message(CommandStart())
 async def cmd_start(message: Message):
-    await message.answer("hello world")
+    await message.answer(f'hello world',reply_markup=kb.main)
 async def main():
     await dp.start_polling(bot)
 
